5fd42185d0115fda49d12f977ec3361241090986
bmwasaru <bmwasaru@gmail.com>
2017-07-11 11:01:59 +0300

    api for discourse

--- drhub/api/discourse.py
+++ drhub/api/discourse.py
@@ -17,6 +17,4 @@ discourse = Blueprint('discourse', __name__, url_prefix='/discourse')
 @discourse.route('/topics/top', methods=['GET'])
 def get_hotests_topics():
     top_topics = discourse_client.top_topics()
-    topics = []
-    for topic in top_topics[0]:
     return jsonify({'messages': top_topics})
